d06e26e3774175062e07d8ba01b065f26a0b7b5b
AlamRidha <alamrridha@gmail.com>
2025-12-25 16:06:37 +0700

    first commit

---
+++ app/global.d.ts
@@ -0,0 +1,29 @@
+// export { };
+
+// declare module '*.glb';
+// declare module '*.png';
+
+// declare module 'meshline' {
+//   export const MeshLineGeometry: any;
+//   export const MeshLineMaterial: any;
+// }
+
+// declare global {
+//   namespace JSX {
+//     interface IntrinsicElements {
+//       meshLineGeometry: any;
+//       meshLineMaterial: any;
+//     }
+//   }
+// }
+import { MeshLineGeometry, MeshLineMaterial } from "meshline";
+import { extend, Object3DNode, MaterialNode } from "@react-three/fiber";
+
+declare module "@react-three/fiber" {
+  interface ThreeElements {
+    meshLineGeometry: Object3DNode<MeshLineGeometry, typeof MeshLineGeometry>;
+    meshLineMaterial: MaterialNode<MeshLineMaterial, typeof MeshLineMaterial>;
+  }
+}
+
+extend({ MeshLineGeometry, MeshLineMaterial });
--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,72 @@
 import Image from "next/image";
+import Lanyard from "./components/Lanyard/Lanyard";
+import RotatingText from "./components/RotatingText/RotatingText";
+import SplitText from "./components/SplitText/SplitText";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="min-h-screen overflow-x-hidden bg-[#19222D]">
+      <div className="container mx-auto border h-screen">
+        <div className="grid grid-cols-12">
+          <div className="col-span-6 border">
+            <div className="flex flex-col gap-4">
+              <div className="flex items-center gap-2">
+                <h1 className="text-2xl font-bold text-white">
+                  Im Ready For Job
+                </h1>
+                <RotatingText
+                  texts={[
+                    "Web Development",
+                    "Web Design",
+                    "System Analyst",
+                    "Front End Developer",
+                  ]}
+                  mainClassName="px-2 sm:px-2 md:px-3 bg-[#C6F10E] text-black overflow-hidden py-0.5 sm:py-1 justify-center rounded-lg text-2xl font-bold inline-flex transition-all"
+                  staggerFrom={"last"}
+                  initial={{ y: "100%" }}
+                  animate={{ y: 0 }}
+                  exit={{ y: "-120%" }}
+                  staggerDuration={0.025}
+                  splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+                  transition={{ type: "spring", damping: 30, stiffness: 400 }}
+                  rotationInterval={2000}
+                />
+              </div>
+
+              <div className="flex flex-col items-start">
+                <SplitText
+                  text="I'm Alam"
+                  className="text-6xl font-semibold text-start"
+                  delay={150}
+                  duration={0.6}
+                  ease="power3.out"
+                  splitType="chars"
+                  from={{ opacity: 0, y: 40 }}
+                  to={{ opacity: 1, y: 0 }}
+                  threshold={0.2}
+                  rootMargin="-50px"
+                />
+
+                <SplitText
+                  text="Web Developer"
+                  className="text-6xl font-semibold text-start text-[#C6F10E]"
+                  delay={75}
+                  duration={0.6}
+                  ease="power3.out"
+                  splitType="chars"
+                  from={{ opacity: 0, y: 40 }}
+                  to={{ opacity: 1, y: 0 }}
+                  threshold={0.2}
+                  rootMargin="-50px"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="col-span-6 border">
+            <Lanyard position={[0, 0, 12]} gravity={[0, -40, 0]} />
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </div>
     </div>
   );
 }
